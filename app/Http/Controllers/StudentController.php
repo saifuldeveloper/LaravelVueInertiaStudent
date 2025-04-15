f26f4b6c30b63e0e758474a1a019f4193e8f2bcd
saifuldeveloper <mdsaiful77336@gmail.com>
2025-04-15 15:37:46 +0600

    Store data

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreStudentRequest;
 use App\Http\Resources\ClassesResouce;
 use App\Http\Resources\StudentResource;
 use App\Models\Classes;
@@ -43,9 +44,11 @@ class StudentController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreStudentRequest $request)
     {
-        //
+            Student::create($request->validated());
+
+            return redirect()->route('students.index');
     }
 
     /**
--- resources/js/Pages/Students/Create.vue
+++ resources/js/Pages/Students/Create.vue
@@ -1,4 +1,5 @@
 <script setup>
+import InputError from '@/Components/InputError.vue';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head, useForm } from '@inertiajs/vue3';
 import axios from 'axios';
@@ -10,7 +11,7 @@ defineProps({
         required: true
     },
 })
-let sections =ref([]);
+let sections = ref([]);
 const form = useForm({
     name: "",
     email: "",
@@ -30,14 +31,15 @@ const getSections = (classId) => {
     });
 };
 
-
+const createStudent = () => {
+    form.post(route("students.store"));
+};
 
 </script>
 
 <template>
 
     <Head title="Create Students" />
-
     <AuthenticatedLayout>
         <template #header>
             <h2 class="text-xl font-semibold leading-tight text-gray-800">
@@ -47,7 +49,7 @@ const getSections = (classId) => {
         <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
             <div class="lg:grid lg:grid-cols-12 lg:gap-x-5">
                 <div class="space-y-6 sm:px-6 lg:px-0 lg:col-span-12">
-                    <form>
+                    <form @submit.prevent="createStudent">
                         <div class="shadow sm:rounded-md sm:overflow-hidden">
                             <div class="bg-white py-6 px-4 space-y-6 sm:p-6">
                                 <div>
@@ -61,38 +63,51 @@ const getSections = (classId) => {
                                 <div class="grid grid-cols-6 gap-6">
                                     <div class="col-span-6 sm:col-span-3">
                                         <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
-                                        <input v-model="form.name" type="text" id="name" class="mt-1 w-full border border-gray-300 rounded-md shadow-sm py-2 px-3
-                                    focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
-                                        <p class="mt-1 text-sm text-red-500">Error Message</p>
+                                        <input v-model="form.name" type="text" id="name"
+                                            class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3
+                                               focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                            :class="{ 'text-red-900 focus:ring-red-500 focus:border-red-500 border-red-400': form.errors.name }" />
+
+                                        <InputError :message="form.errors.name" class="mt-2" />
+
+
                                     </div>
 
                                     <div class="col-span-6 sm:col-span-3">
                                         <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
                                         <input v-model="form.email" type="email" id="email" class="mt-1 w-full border border-gray-300 rounded-md shadow-sm py-2 px-3
-                                    focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
-                                    
-                                        <p class="mt-1 text-sm text-red-500">Error Message</p>
+                                    focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                            :class="{ 'text-red-900 focus:ring-red-500 focus:border-red-500 border-red-400': form.errors.email }" />
+
+                                        <InputError :message="form.errors.email" class="mt-2" />
                                     </div>
                                     <div class="col-span-6 sm:col-span-3">
                                         <label for="class_id"
                                             class="block text-sm font-medium text-gray-700">Class</label>
                                         <select v-model="form.class_id" id="class_id" class="mt-1 w-full border border-gray-300 rounded-md shadow-sm py-2 px-3
-                                    focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                    focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                            :class="{ 'text-red-900 focus:ring-red-500 focus:border-red-500 border-red-400': form.errors.class_id }">
                                             <option value="">Select a class</option>
                                             <option v-for="item in classes.data" :key="item.id" :value="item.id">{{
                                                 item.name }}
                                             </option>
 
                                         </select>
+                                        <InputError :message="form.errors.class_id" class="mt-2" />
                                     </div>
                                     <div class="col-span-6 sm:col-span-3">
                                         <label for="section_id"
                                             class="block text-sm font-medium text-gray-700">Section</label>
                                         <select v-model="form.section_id" id="section_id" class="mt-1 w-full border border-gray-300 rounded-md shadow-sm py-2 px-3
-                                    focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                    focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                            :class="{ 'text-red-900 focus:ring-red-500 focus:border-red-500 border-red-400': form.errors.section_id }">
                                             <option value="">Select a Section</option>
-                                            <option v-for="section in sections.data" :key="section.id"  :value="section.id">{{ section.name }}</option>
+                                            <option v-for="section in sections.data" :key="section.id"
+                                                :value="section.id">{{
+                                                    section.name }}</option>
                                         </select>
+
+                                        <InputError :message="form.errors.section_id" class="mt-2" />
                                     </div>
                                 </div>
                             </div>
